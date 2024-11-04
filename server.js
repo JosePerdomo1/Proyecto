746c39c1ca9e7c99f4c5ce4eab5970bd2f2e06cf
JosePerdomo1 <125402882+JosePerdomo1@users.noreply.github.com>
2024-11-04 21:37:30 +0100

    funcionando local

--- server.js
+++ server.js
@@ -2,15 +2,33 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+let latestTemperature = null; // Variable para almacenar la última temperatura recibida
+
 // Endpoint para recibir datos de temperatura
 app.get('/temperature', (req, res) => {
-  const temperature = parseFloat(req.query.temp);  // Leer el parámetro "temp" de la URL
+  const temperature = parseFloat(req.query.temp); // Leer el parámetro "temp" de la URL
   if (!isNaN(temperature)) {
+    latestTemperature = temperature; 
     console.log(`Temperatura recibida: ${temperature}°C`);
-    // Aquí podrías guardar los datos en la base de datos, si lo deseas.
     res.send('Temperatura recibida');
   } else {
     res.status(400).send('Temperatura inválida');
+  }
+});
+
+// Endpoint para servir la página web
+app.get('/', (req, res) => {
+  const path = require('path');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  
+});
+
+// Endpoint para obtener la última temperatura
+app.get('/getTemperature', (req, res) => {
+  if (latestTemperature !== null) {
+    res.json({ temperature: latestTemperature }); // Envía la temperatura como JSON
+  } else {
+    res.status(404).json({ error: 'No hay datos de temperatura disponibles' });
   }
 });
 
